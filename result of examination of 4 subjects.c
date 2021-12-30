057264260ada9038b5ef8e7f7a438067fa3a229b
divi134 <96563541+divi134@users.noreply.github.com>
2021-12-30 11:47:13 +0530

    Update result of examination of 4 subjects.c

--- result of examination of 4 subjects.c
+++ result of examination of 4 subjects.c
@@ -4,7 +4,7 @@ void main()
 	int M,P,C,CS;
 	printf("Enter marks:");
 	scanf("%d%d%d%d",&M,&P,&C,&CS);
-	if(M>35 && P>35 && C>35 && CS>35)
+	if(M>=35 && P>=35 && C>=35 && CS>=35)
 	{
 		printf("Pass");
 	}
