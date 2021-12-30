2824617278313f9b3b70b2f6a61b4537401d844d
divi134 <96563541+divi134@users.noreply.github.com>
2021-12-30 10:31:50 +0530

    Add files via upload

---
+++ divisibility of number.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+main()
+{
+	int number;
+	printf("Number:");
+	scanf("%d",&number);
+	if(number%1==0)
+	{
+		printf("divisible by 1");
+	}
+	else 
+	{
+		printf("\nnot divisible by 1");
+	}
+	if(number%2==0)
+	{
+		printf("\ndivisible by 2");
+	}
+	else 
+	{
+		printf("\nnot divisible by 2");
+	}
+    if(number%3==0)
+	{
+		printf("\ndivisible by 3");
+	}
+	else
+	{
+		printf("\nnot divisible by 3");
+	}
+	 if(number%4==0)
+	{
+		printf("\ndivisible by 4");
+	}
+	else
+	{
+		printf("\nnot divisible by 4");
+	}
+	 if(number%5==0)
+	{
+		printf("\ndivisible by 5");
+	}
+	else
+	{
+		printf("\nnot divisible by 5");
+	}
+	 if(number%6==0)
+	{
+		printf("\ndivisible by 6");
+	}
+	else
+	{
+		printf("\nnot divisible by 6");
+	}
+	 if(number%7==0)
+	{
+		printf("\ndivisible by 7");
+	}
+	else
+	{
+		printf("\nnot divisible by 7");
+	}
+	 if(number%8==0)
+	{
+		printf("\ndivisible by 8");
+	}
+	else
+	{
+		printf("\nnot divisible by 8");
+	}
+	 if(number%9==0)
+	{
+		printf("\ndivisible by 9");
+	}
+	else
+	{
+		printf("\nnot divisible by 9");
+	}
+}
