9c2048b1c1031ffa9c95a823e37efa4b1084a629
divi134 <96563541+divi134@users.noreply.github.com>
2021-12-30 10:53:00 +0530

    Update largest of 4 numbers.c

--- largest of 4 numbers.c
+++ largest of 4 numbers.c
@@ -6,18 +6,18 @@ main()
 	scanf("%d%d%d%d",&a,&b,&c,&d);
 	if(a>b && a>c && a>d)
 	{
-		printf("\n%d,a");
+		printf("\n%d",a);
 	}
-	if(b>a && b>c && b>d)
+	else if(b>a && b>c && b>d)
 	{
-		printf("\n%d,b");
+		printf("\n%d",b);
 	}
-	if(c>a && c>b && c>d)
+	else if(c>a && c>b && c>d)
 	{
-		printf("\n%d,c");
+		printf("\n%d",c);
 	}
-	if(d>a && d>b && d>c)
+	else
 	{
-		printf("\n%d,d");
+		printf("\n%d",d);
 	}
 }
